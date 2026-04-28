e3364a2e174a98524b616a128fb93f4f867f974b
Robert Madsen <robert.t.madsen13@gmail.com>
2026-04-28 16:44:10 -0400

    chore: cleanup temp files

--- push_fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\Rob\Documents\Greenskeeper Studios\Bova Invitational"
-"C:\Program Files\Git\cmd\git.exe" add -A
-"C:\Program Files\Git\cmd\git.exe" commit -m "fix: root URL now serves marketing page"
-"C:\Program Files\Git\cmd\git.exe" push origin main
-echo DONE
-pause
